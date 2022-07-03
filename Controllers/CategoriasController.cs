204767a2e0fdfeef4b87a8216401d7ef786f2dc1
Vinícius <iits7rue@gmail.com>
2022-07-03 11:02:14 -0300

    products endpoints and basic categories endpoints

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,69 @@
+using catalogo_api.Context;
+using catalogo_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace catalogo_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly CatalogoContext _catalogoContext;
+
+        public CategoriasController(CatalogoContext catalogoContext)
+        {
+            _catalogoContext = catalogoContext;
+        }
+        
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _catalogoContext.Categorias?.Find(id);
+            return categoria is null ? NotFound() : categoria;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categorias = _catalogoContext.Categorias?.ToList();
+            return categorias is null ? NotFound() : categorias;
+        }
+
+        [HttpPost]
+        public ActionResult<Categoria> Post(Categoria categoria)
+        {
+            if(categoria is null) return BadRequest();
+
+            _catalogoContext.Categorias?.Add(categoria);
+            _catalogoContext.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId, categoria});
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<Categoria> Put(int id, Categoria categoria)
+        {
+            if(categoria is null || categoria.CategoriaId != id) return BadRequest();
+
+            _catalogoContext.Entry<Categoria>(categoria).State = EntityState.Modified;
+            _catalogoContext.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _catalogoContext.Categorias?.FirstOrDefault(c => c.CategoriaId == id);
+
+            if(categoria is null) return NotFound();
+
+            _catalogoContext.Categorias?.Remove(categoria);
+            _catalogoContext.SaveChanges();
+
+            return Ok(categoria);
+
+        }
+    }
+}
--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 using catalogo_api.Context;
 using catalogo_api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace catalogo_api.Controllers
 {
@@ -38,6 +39,40 @@ namespace catalogo_api.Controllers
 
             return new CreatedAtRouteResult("ObterProduto", 
             new { id = produto.ProdutoId}, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if(produto is null || id != produto.ProdutoId) return BadRequest();
+
+            // var produtoProcurado = _catalogoContext.Produtos?.Find(produto.ProdutoId);
+
+            // if(produtoProcurado is null) return NotFound();
+
+            // _catalogoContext.Update(produto);
+            // _catalogoContext.Attach(produto);
+            _catalogoContext.Entry(produto).State = EntityState.Modified;
+            _catalogoContext.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            //Pesquisa diretamente no banco;
+            var produto = _catalogoContext.Produtos?.FirstOrDefault(p => p.ProdutoId == id);
+            
+            //Pesquisa primeiro na memória, oq pode gerar erros em alguns momentos;
+            // var produto = _catalogoContext.Produtos?.Find(id);
+
+            if(produto is null) return NotFound();
+
+            _catalogoContext.Produtos?.Remove(produto);
+            _catalogoContext.SaveChanges();
+            
+            return Ok(produto);
         }
     }
 }
